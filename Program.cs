ab8509edfbac699179dd8cf5bc9d5ad87b2c61b5
Александра <112776816+SaiALL441@users.noreply.github.com>
2023-03-18 15:48:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4;
+
+public class ClassCounter
+{
+    public delegate void MessageWrite();
+    public event MessageWrite onCount;
+
+    public void Count()
+    {
+        for (int i=0; i<25; i++)
+        {
+            if (i == 15)
+            {
+                onCount();
+            }
+        }
+    }
+}
+
+
+class Time_I 
+{
+    public void Message()
+    {
+        Console.WriteLine("15 секунд до старту");
+    }
+}
+class Time_II
+{
+    public void Message()
+    {
+        Console.WriteLine("5 секунд до старту");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ClassCounter Counter = new ClassCounter();
+        Time_I Time1 = new Time_I();
+        Time_II Time2 = new Time_II();
+
+        Counter.onCount += Time1.Message;
+        Counter.onCount -= Time2.Message;
+
+        Counter.Count();
+    }
+}
